3caada923e592e60d2ab50a4af7098df8d56da7c
Gabriele Scalici <38985346+gabacode@users.noreply.github.com>
2022-03-20 15:20:03 +0100

    get user id from qrcodeUUID

--- src/main/webapp/app/qrscan/qrscan.component.ts
+++ src/main/webapp/app/qrscan/qrscan.component.ts
@@ -1,26 +1,42 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { BarcodeFormat } from '@zxing/library';
+import { IRefugee } from 'app/entities/refugee/refugee.model';
+import { RefugeeService } from 'app/entities/refugee/service/refugee.service';
 
-interface QRScan {
-  uuid: string;
-}
 @Component({
   selector: 'qrscan',
   templateUrl: './qrscan.component.html',
   styleUrls: ['./qrscan.component.scss']
 })
 export class QrScanComponent {
-
+  isLoading: boolean = false;
   scannedQr: string = ''
+  match?: IRefugee['id'];
   allowedFormats = [ BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX ];
   scannerEnabled: boolean = true;
 
-  constructor() {}
+  constructor(protected refugeeService: RefugeeService) {}
+
+  findQR(qrcodeUUID: IRefugee['qrcodeUUID']): void {
+    this.isLoading = true;
+    this.refugeeService.findByUUID(qrcodeUUID!).subscribe({
+      next: (res: HttpResponse<IRefugee>) => {
+        const id = res.body!['id'];
+        this.match = id;
+        console.log(this.match);
+        this.isLoading = false;
+      },
+      error: () => {
+        console.log('No match.');
+        this.isLoading = false;
+      },
+    });
+  }
 
   public scanSuccessHandler($event: string) {
-    this.scannedQr = $event;
-    const qr: QRScan = { uuid: $event };
-    console.log(qr);
+    const qrcodeUUID: IRefugee['qrcodeUUID'] = $event;
+    this.findQR(qrcodeUUID);
     this.scannerEnabled = false;
   }
 
