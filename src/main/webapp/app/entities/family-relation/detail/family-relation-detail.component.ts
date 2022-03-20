0d47063c8037b79c1ddb248ed7db12c70479b2a6
Salvatore Pilò <satowine@gmail.com>
2022-03-20 14:12:53 +0100

    Aggiornato print per il refugee

--- src/main/webapp/app/entities/family-relation/detail/family-relation-detail.component.ts
+++ src/main/webapp/app/entities/family-relation/detail/family-relation-detail.component.ts
@@ -9,12 +9,20 @@ import { IFamilyRelation } from '../family-relation.model';
 })
 export class FamilyRelationDetailComponent implements OnInit {
   familyRelation: IFamilyRelation | null = null;
+  refugee1ViewData: string | unknown = null;
+  refugee2ViewData: string | unknown = null;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ familyRelation }) => {
       this.familyRelation = familyRelation;
+      this.refugee1ViewData = (familyRelation.refugee1.personalInformation.firstName || familyRelation.refugee1.personalInformation.lastName)
+                              ? familyRelation.refugee1?.personalInformation?.firstName + ' ' + familyRelation.refugee1?.personalInformation?.lastName
+                              : familyRelation.refugee1?.personalInformation?.id;
+      this.refugee2ViewData = (familyRelation.refugee2.personalInformation.firstName || familyRelation.refugee2.personalInformation.lastName)
+                              ? familyRelation.refugee2?.personalInformation?.firstName + ' ' + familyRelation.refugee2?.personalInformation?.lastName
+                              : familyRelation.refugee2?.personalInformation?.id;
     });
   }
 
